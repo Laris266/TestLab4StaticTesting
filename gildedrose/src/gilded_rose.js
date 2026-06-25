b3aa9ae47d52a45eddaed6da6d8568b216441235
Laris266 <if25b266@technikum-wien.at>
2026-06-25 17:26:49 +0200

    all tests green

--- gildedrose/src/gilded_rose.js
+++ gildedrose/src/gilded_rose.js
@@ -4,14 +4,17 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-const items = [
-  new Item('+5 Dexterity Vest', 10, 20),
-  new Item('Aged Brie', 2, 0),
-  new Item('Elixir of the Mongoose', 5, 7),
-  new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-  new Item('Conjured Mana Cake', 3, 6),
-];
+let items = [];
+
+items.push(
+  new Item("+5 Dexterity Vest", 10, 20),
+  new Item("Aged Brie", 2, 0),
+  new Item("Elixir of the Mongoose", 5, 7),
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Conjured Mana Cake", 3, 6)
+);
+
 function update_quality() {
 
  for(const item of items) {
